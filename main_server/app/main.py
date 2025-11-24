6f07db299c7e5c295c759fecc0128b7e998c2581
myoungjugo <rinaroo0512@naver.com>
2025-11-24 17:08:30 +0900

    Revert "Merge branch 'feat/add-router-and-envfile' of https://github.com/kwang-i-coder/25-26_PT_team2_BE into feature/mail_server"
    This reverts commit 463025a1fdfaa437baac762973a27037ff2afc75, reversing
    changes made to 26be2a138c9845de6af48b180524f3ae9cf88f1f.

--- main_server/app/main.py
+++ main_server/app/main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
 from app.routers.auth_router import auth_router
-from app.routers.jandi_router import jandi_router
-from app.routers.platform_router import platform_router
-from app.routers.user_router import user_router
 
 app = FastAPI()
 app.include_router(router=auth_router)
-app.include_router(router=jandi_router)
-app.include_router(router=platform_router)
-app.include_router(router=user_router)
 
 @app.get("/")
 async def root():
--- main_server/app/routers/platforn_router.py
+++
@@ -1,3 +0,0 @@
-from fastapi import APIRouter   
-
-platform_router = APIRouter(prefix='/platform')
